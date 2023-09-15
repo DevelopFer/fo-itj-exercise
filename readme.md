fa2d70e86625eb686f4c511281ef1eaf8035e1ac
Fernando Ordoñez <fernando@thiio.com>
2023-09-14 22:17:00 -0500

    finishing project

---
+++ readme.md
@@ -0,0 +1,48 @@
+
+## Installation
+
+Follow this steps for installing the project
+
+```bash
+  cd my-project
+```
+Install all required dependencies
+```bash
+  npm install my-project
+```
+Compile files using typescript
+```bash
+  npx tsc
+```
+Run!
+```bash
+  npm run assignment --addressFilePath=./addresses.txt --driversFilePath=./drivers.txt
+```
+
+Output Example
+```bash
+Max SS: 166
+┌─────────┬───────────────────────────────────────────────────────┬───────────────────┐
+│ (index) │                           0                           │         1         │
+├─────────┼───────────────────────────────────────────────────────┼───────────────────┤
+│    0    │       '215 Osinski Manors, San Diego, CA 92126'       │  'Cleve Durgan'   │
+│    1    │     '9856 Marvin Stravenue, San Diego, CA 92126'      │ 'Howard Emmerich' │
+│    2    │       '7127 Kathlyn Ferry, San Diego, CA 92126'       │ 'Everardo Welch'  │
+│    3    │       '987 Champlin Lake, San Diego, CA 92126'        │   'Izaiah Lowe'   │
+│    4    │ '63187 Volkman Garden Suite 447, San Diego, CA 92126' │  'Orval Mayert'   │
+│    5    │      '75855 Dessie Lights, San Diego, CA 92126'       │   'Kaiser Sose'   │
+│    6    │   '1797 Adolf Island Apt. 744, San Diego, CA 92126'   │  'Ellis Wisozk'   │
+│    7    │     '2431 Lindgren Corners, San Diego, CA 92126'      │ 'Murphy Mosciski' │
+│    8    │ '8725 Aufderhar River Suite 859, San Diego, CA 92126' │ 'Monica Hermann'  │
+│    9    │         '1234 Fake St., San Diego, CA 92126'          │  'Noemie Murphy'  │
+└─────────┴───────────────────────────────────────────────────────┴───────────────────┘
+```
+
+## Running Tests
+
+To run tests, run the following command
+
+```bash
+  npm run test
+```
+
---
+++ tests/unit.test.js
@@ -0,0 +1,175 @@
+import {it, expect, describe, beforeEach, expectTypeOf, beforeAll} from 'vitest';
+import { faker } from '@faker-js/faker';
+import { ShipmentManager } from '../dist/shipmentmanager';
+import { List } from "../dist/list";
+
+let shipmentManager;
+
+const totalFakeAddresses = 10;
+const totalFakeDrivers = 10;
+
+beforeAll( () => {
+    
+    shipmentManager = new ShipmentManager();
+    
+});
+
+describe("Unit Tests", () => {
+
+    it("should instanciate a ShipmenManager", () => {
+        
+        expect(shipmentManager.constructor.name).toEqual("ShipmentManager");
+
+    });
+    
+    it('should generate combinations',async () => {
+
+        const letters = ['a','e','i','o','u'];
+        const numbers = ["1","2","3","4","5"];
+        const combinations = [
+            'a1', 'a2', 'a3', 'a4', 'a5',
+            'e1', 'e2', 'e3', 'e4', 'e5',
+            'i1', 'i2', 'i3', 'i4', 'i5',
+            'o1', 'o2', 'o3', 'o4', 'o5',
+            'u1', 'u2', 'u3', 'u4', 'u5'
+        ];
+        const dictionary = [];
+        
+        const buildCombinations = (arr1, arr2, dictionary, callback) => {
+            const currentElement = arr1.shift();
+            for( let element of arr2 ){
+                dictionary.push(`${currentElement}${element}`);
+            }
+            if( ! arr1.length ) return dictionary;
+            return buildCombinations(arr1,arr2, dictionary);
+        };
+        const result = buildCombinations(letters, numbers, dictionary);
+        
+        expect(combinations).toMatchObject(result);
+
+    });
+
+    const generateAddresses = (number) => {
+        const addresses = [];
+        for(let i = 0; i < number; i++){
+            const streeName = faker.address.streetAddress();
+            const zipCode   = faker.address.zipCode();
+            const state     = faker.address.state();
+            const city      = faker.address.city();
+            addresses.push(`${streeName}, ${city}, ${state} ${zipCode}`);
+        }
+        return addresses;
+    };
+
+    const generateDrivers = (number) => {
+        const names = [];
+        for(let i = 0; i < number; i++){
+            const name = faker.name.fullName();
+            names.push(name);
+        }
+        return names;
+    };
+
+
+    it("should return only street name from an address", () => {
+        
+        const streeName = faker.address.streetAddress();
+        const zipCode   = faker.address.zipCode();
+        const state     = faker.address.state();
+        const city      = faker.address.city();
+        const address = `${streeName}, ${city}, ${state} ${zipCode}`;
+
+        const onlyStreetName = shipmentManager.getStreetName(address);
+
+        expect(onlyStreetName).toEqual(streeName);
+
+    });    
+
+    it("should return true if an string has an even length", () => {
+        
+        const demoString = "thisisanevenstring";
+        
+        const isEven = shipmentManager.lengthIsEven(demoString);
+        
+        expect(isEven).toBe(true);
+    });
+
+    it("should return false if an string has an odd length", () => {
+        
+        const demoString = "thisisanoddstring";
+        
+        const isEven = shipmentManager.lengthIsEven(demoString);
+        
+        expect(isEven).toBe(false);
+    });
+
+    it("should count only vowels", () => {
+        
+        const stringWithVowels = "MuRcIElaGO";
+        
+        const totalVowels = shipmentManager.countVowels(stringWithVowels);
+        
+        expect(totalVowels).toBe(5);
+    });
+
+    it("should count only consonants", () => {
+        
+        const stringWithConsonants = "CIrcUNSCribir";
+        
+        const totalConsonants = shipmentManager.countConsonants(stringWithConsonants);
+        
+        expect(totalConsonants).toBe(9);
+
+    });
+
+    it("should count factors", () => {
+        
+        //20 => [2,4,5,10,20]
+        const stringChain = "istwentylengthstring";
+        
+        const totalFactors = shipmentManager.countFactors(stringChain);
+        
+        expect(Object.keys(totalFactors).length).toBe(5);
+
+    });
+
+    it("should build a new list", () => {
+
+        const demoString = "12345678";
+        const mylist = new List();
+        mylist.add(1);
+        mylist.add(2);
+        mylist.add(3);
+        mylist.add(4);
+        mylist.add(5);
+        mylist.add(6);
+        mylist.add(7);
+        mylist.add(8);
+        
+        expect(mylist.print()).toEqual(demoString);
+
+    });
+
+
+    it("should generate permutations", async () => {
+        
+        const elements = ["a","b","c","d","e","f","g","h","i"];
+
+        await shipmentManager.generatePermutations(elements);
+        
+        expect(shipmentManager.permutations.length).toBe(362880);
+        
+    });
+
+
+    it("should generate permutations using drivers", async () => {
+        
+        const elements = generateDrivers(3);
+
+        await shipmentManager.generatePermutations(elements);
+      
+        expect(shipmentManager.permutations.length).toBe(6);
+        
+    });
+
+});
